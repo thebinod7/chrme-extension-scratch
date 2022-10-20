65c794196add1e6931fd08aaf7b741ca41c3f49a
Binod Chaudhary <thebinod7@gmail.com>
2022-10-20 15:43:49 +0545

    store tokens and create board item

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createRoot } from "react-dom/client";
 
 import './App.scss';
@@ -18,6 +18,7 @@ function Popup() {
     const [accountConnected, setAccountConnected] = useState(false);
 
     const fetchBoardData = (access_token) => {
+        if(!access_token) return console.log("Authorize app first!");
         fetch(MONDAY_API_ENDPOINT, {
             method: 'post',
             headers: {
@@ -25,15 +26,30 @@ function Popup() {
                 'Authorization' : access_token
             },
             body: JSON.stringify({
-                'query' : '{ boards (limit:5) {id name} }'
+                'query' : '{ boards (limit:5) {id name groups{ id title} } }'
             })
         })
         .then(async(res) => {
             const data = await res.json();
             console.log("API RES==>",data.data.boards);
+            setAccountConnected(true);
             setBoards(data.data.boards); 
         }).catch(err => console.log("API ERR:", err));
     }
+
+    const saveAccessToken = (access_token) => {
+        chrome.storage.local.set({"monday_access_token": access_token}, function() {
+            console.log('Value is set to ' + access_token);
+          });
+    };
+
+    const getTokenAndFetchBoads = async () => {
+        chrome.storage.local.get(['monday_access_token'], function(result) {
+            if(result.monday_access_token) setAccountConnected(true);
+            fetchBoardData(result.monday_access_token);
+            return result.monday_access_token;
+          });
+    };
 
     const handleConnectMonday = () => {
         chrome.identity.launchWebAuthFlow({'url':AUTH_URL,'interactive':true}, function(redirect_url){
@@ -54,12 +70,55 @@ function Popup() {
                 body: new URLSearchParams(data)
               }).then(async(res) => {
                 const d = await res.json();
-                setAccountConnected(true);
+                saveAccessToken(d.access_token);
                 return fetchBoardData(d.access_token)
               }).catch(err => console.log("Err==>",err))
-      
-            // Fetch boards using API
         });
+    }
+
+    useEffect(() => {
+        getTokenAndFetchBoads();
+    },[]);
+
+    const handleDisconnectClick = () => {
+        chrome.storage.local.clear();
+        setAccountConnected(false);
+    };
+
+    const handleCreateItem = (e) => {
+        e.preventDefault();
+        const form = new FormData(e.target);
+        const itemName = form.get('name');
+        const boardId = form.get('boardId');
+        return saveBoardItem({boardId, itemName}, e)
+    }
+
+    const getAccessToken = async() => {
+        const {monday_access_token} = await chrome.storage.local.get(['monday_access_token']);
+        if(!monday_access_token) console.log("No access token");
+        return monday_access_token;
+    }
+
+    const saveBoardItem = async(payload, e) => {
+        const access_token = await getAccessToken();
+        let query = `mutation { create_item (board_id: ${payload.boardId}, group_id: \"topics\", item_name: \"${payload.itemName}\") { id }}`;
+
+            fetch (MONDAY_API_ENDPOINT, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization' : access_token
+            },
+            body: JSON.stringify({
+                query : query
+            })
+            })
+        .then(res => {
+            res.json();
+            alert("Item created successfully!");
+            e.target.reset();
+        })
+        .then(res => console.log(JSON.stringify(res, null, 2)));
     }
 
     return (
@@ -68,15 +127,21 @@ function Popup() {
             {/* <div>
                 <img src={RenderingImg} alt="Render Arch" />
             </div> */}
-            {accountConnected ? <p>Account Connected!</p> : <button onClick={handleConnectMonday}>Connect to Monday</button>}
+            {accountConnected ? <div> <p>Account Connected!</p> <button onClick={handleDisconnectClick}>Disconnect Account</button></div> : <button onClick={handleConnectMonday}>Connect to Monday</button>}
 
             <hr />
             <div>
                 <h4>Connected board list:</h4>
-                {boards.length > 0 ? boards.map((b) => {
+                {accountConnected && boards.length > 0 ? boards.map((b) => {
                     return <p key={b.id}>&#9734; Name: <strong>{b.name}</strong>, ID: <strong>{b.id}</strong></p>
                 }) : 'No boards to display!'}
             </div>
+
+            <form onSubmit={handleCreateItem}>
+                <input type="text" name="name" placeholder='Enter item name' required />
+                <input type="text" name="boardId" placeholder='Enter board ID' required />
+                <button type='submit'>Create Item</button>
+            </form>
         </div>
     )
 }
